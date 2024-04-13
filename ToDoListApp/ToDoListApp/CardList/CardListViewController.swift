7c0b0c9d584abe94a4fbf4c504faec666ae42b7c
joho2022 <josama1997@naver.com>
2024-04-14 02:20:51 +0900

    feat: 헤더뷰의 + 버튼을 누르면, 각 주제에 맞는 테이블에 셀 생성

--- ToDoListApp/ToDoListApp/CardList/CardListViewController.swift
+++ ToDoListApp/ToDoListApp/CardList/CardListViewController.swift
@@ -7,20 +7,43 @@
 
 import UIKit
 
+extension Notification.Name {
+    static let NewCardAdded = Notification.Name("NewCardAdded")
+}
+
 class CardListViewController: UIViewController {
     var tableView: UITableView!
     var stackView: UIStackView!
+    var cardManager: CardManager
+    var headerView: CardListHeaderView!
     let headerTitle: String
-    var cards: [ToDoCard]
+    let cardStatus: CardStatus
     
-    init(headerTitle: String, cards: [ToDoCard] = []) {
+    init(headerTitle: String, cardManager: CardManager, cardStatus: CardStatus) {
         self.headerTitle = headerTitle
-        self.cards = cards
+        self.cardManager = cardManager
+        self.cardStatus = cardStatus
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        self.headerTitle = ""
+        self.cardManager = CardManager()
+        self.cardStatus = .toDO
+        super.init(coder: coder)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleNewCardAdded),
+                                               name: .NewCardAdded,
+                                               object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: .NewCardAdded, object: nil)
     }
     
     override func viewDidLoad() {
@@ -44,13 +67,27 @@ class CardListViewController: UIViewController {
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         
-        let headerView = CardListHeaderView()
+        headerView = CardListHeaderView()
         headerView.titleLabel.text = headerTitle
-        headerView.badgeLabel.text = "\(cards.count)"
+        updateHeaderBadge()
+        headerView.addButtonAction = { [weak self] in
+            self?.addNewCard()
+        }
         headerView.translatesAutoresizingMaskIntoConstraints = false
         stackView.addArrangedSubview(headerView)
         
         headerView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+    }
+    
+    private func updateHeaderBadge() {
+        headerView.badgeLabel.text = "\(cardManager.cards(for: cardStatus).count)"
+    }
+    
+    private func addNewCard() {
+        let editVC = EditViewController(cardManager: cardManager, cardStatus: self.cardStatus)
+        editVC.modalPresentationStyle = .overFullScreen
+        
+        self.present(editVC, animated: true)
     }
     
     private func setupTableView() {
@@ -68,17 +105,23 @@ class CardListViewController: UIViewController {
         let nib = UINib(nibName: "CardTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "CardTableViewCell")
     }
+    
+    // MARK: - Notification Handlers
+    @objc func handleNewCardAdded(notification: Notification) {
+        updateHeaderBadge()
+        self.tableView.reloadData()
+    }
 }
 
 extension CardListViewController: UITableViewDataSource, UITableViewDelegate {
     // MARK: - dataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cards.count
+        return cardManager.cards(for: cardStatus).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CardTableViewCell", for: indexPath) as! CardTableViewCell
-        let card = cards[indexPath.row]
+        let card = cardManager.cards(for: cardStatus)[indexPath.row]
         cell.configure(with: card)
         return cell
     }
@@ -89,5 +132,3 @@ extension CardListViewController: UITableViewDataSource, UITableViewDelegate {
         print("\(headerTitle): \(indexPath.row)")
     }
 }
-
-
